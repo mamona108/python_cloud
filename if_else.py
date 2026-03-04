d3be2bb4b7a608f9c886a899cbd944b9820f3e93
mamona <mamonadaud@gmail.com>
2026-03-04 14:44:28 +0500

    this is loop

---
+++ if_else.py
@@ -0,0 +1,11 @@
+age = 20
+
+if age >= 18:
+    print("Aap adult ho.")
+else:
+    print("Aap minor ho.")
+
+if Number > 2:
+    print("number positive ha")
+else:
+    print("number negative ha ya zero")    
